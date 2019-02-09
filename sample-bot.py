edc54e3b45dd7994c16e6218a1a6a922f0f1e360
Alex Gao <alexgao12@gmail.com>
2019-02-09 13:45:42 -0800

    lol

--- sample-bot.py
+++ sample-bot.py
@@ -19,7 +19,7 @@ import random
 team_name="gettingthingsdone"
 # This variable dictates whether or not the bot is connecting to the prod
 # or test exchange. Be careful with this switch!
-test_mode = False
+test_mode = True
 
 # This setting changes which test exchange is connected to.
 # 0 is prod-like
