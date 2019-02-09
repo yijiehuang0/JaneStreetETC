626630097e88fb29dca75848b4017f9d93deb248
Kevin Zhu <zhu.kevin@berkeley.edu>
2019-02-09 15:53:27 -0800

    1001

--- sample-bot.py
+++ sample-bot.py
@@ -12,7 +12,7 @@ import random
 team_name="gettingthingsdone"
 # This variable dictates whether or not the bot is connecting to the prod
 # or test exchange. Be careful with this switch!
-test_mode = False
+test_mode = True
 
 # This setting changes which test exchange is connected to.
 # 0 is prod-like
@@ -83,12 +83,12 @@ def TradeBond(exchange):
     if data['type'] == 'book' and data['symbol'] == 'BOND':
         bids = data['buy']
         for price, size in bids:
-            if price > 1000:
+            if price > 1001:
                 trades.append(('SELL', 'BOND', price, size))
 
         asks = data['sell']
         for price, size in asks:
-            if price < 1000:
+            if price < 999:
                 trades.append(('BUY', 'BOND', price, size))
     return trades
 
