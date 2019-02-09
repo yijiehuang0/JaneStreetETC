77bb3f723b846fec857406ad1151eab4d8dc2aa4
Kevin Zhu <zhu.kevin@berkeley.edu>
2019-02-09 15:58:00 -0800

    flipped it

--- sample-bot.py
+++ sample-bot.py
@@ -83,14 +83,14 @@ def TradeBond(exchange):
     if data['type'] == 'book' and data['symbol'] == 'BOND':
         bids = data['buy']
         for price, size in bids:
-            if price > 1000:
+            if price < 1000:
                 trades.append(('SELL', 'BOND', price, size))
 
         asks = data['sell']
         for price, size in asks:
-            if price < 1000:
+            if price > 1000:
                 trades.append(('BUY', 'BOND', price, size))
-    return trades
+    return trade
 
 def updateValues(data, symb):
     buys = data['buy']
